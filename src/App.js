107598a3d4f4cb1a801157a2906e0660ec9de1eb
Son Nguyen <nhuson2306@gmail.com>
2022-05-10 19:04:11 +0700

    Learning reactjs with useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const orders = [100, 200, 300];
 
 function App() {
+  const [counter, setCounter] = useState(() => {
+    const totalOrder = orders.reduce((total, current) => total + current);
+
+    return totalOrder;
+  });
+
+  const handleIncrease = () => {
+    setCounter(counter + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 20 }}>
+      <h1>{counter}</h1>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   );
 }
