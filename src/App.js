c390d20eba00811904b207b5670bfb02a4ccd523
Son Nguyen <nhuson2306@gmail.com>
2022-05-10 22:34:03 +0700

    Todolist with useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import { useState } from "react";
 
-const orders = [100, 200, 300];
-
 function App() {
-  const [counter, setCounter] = useState(() => {
-    const totalOrder = orders.reduce((total, current) => total + current);
-
-    return totalOrder;
+  const [job, setJob] = useState("");
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem("jobs"));
+    return storageJobs ?? [];
   });
 
-  const handleIncrease = () => {
-    setCounter(counter + 1);
+  const handleAddJob = (event) => {
+    event.preventDefault();
+
+    setJobs((prevJob) => {
+      const newJobs = [...prevJob, job];
+
+      // Save to local storage
+      const jsonJobs = JSON.stringify(newJobs);
+      localStorage.setItem("jobs", jsonJobs);
+
+      return newJobs;
+    });
+    setJob("");
   };
 
   return (
-    <div className="App" style={{ padding: 20 }}>
-      <h1>{counter}</h1>
-      <button onClick={handleIncrease}>Increase</button>
+    <div className="App" style={{ padding: 32 }}>
+      <form noValidate>
+        <input
+          type="text"
+          value={job}
+          onChange={(event) => setJob(event.target.value)}
+        />
+        <button type="submit" onClick={(event) => handleAddJob(event)}>
+          Add
+        </button>
+      </form>
+
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}</li>
+        ))}
+      </ul>
     </div>
   );
 }
